aae14b1567487a740e3e842cf5816ca77ed438b6
Nills Maillet <mailletnills@gmail.com>
2026-04-07 14:24:27 +0200

    done: Exercice 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    // Exercice 1
+    Voiture v1 = new Voiture("Peugeot", "208", 2021, 14500);
+
+    System.out.println("Marque : " + v1.marque);
+    System.out.println("Modèle : " + v1.modele);
+    System.out.println("Année : " + v1.annee);
+    System.out.println("Prix : " + v1.prix + "€");
+
+}
---
+++ src/Voiture.java
@@ -0,0 +1,13 @@
+public class Voiture {
+    String marque;
+    String modele;
+    int annee;
+    double prix;
+
+    public Voiture(String marque, String modele, int annee, double prix){
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+        this.prix = prix;
+    }
+}
